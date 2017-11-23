3a3bcfa6d99a9f0b742d1ca0628d49b3c41e7d78
Tino Kallinich <tkallinich@inovex.de>
2017-11-23 14:23:09 +0100

    first steps in Cplusplus, declaration of function on top of file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+// declaration of functions on top
+void cOutTest();
+void variablesTest();
+
+
 // using standard library
 using namespace std;
 
@@ -9,7 +14,32 @@ using namespace std;
  * @return
  */
 int main() {
+    // 1. C OUT STREAM
+    cOutTest();
+
+
+    // 2. Variables
+    variablesTest();
+
+    // 8. If Statments
+    cout << " 8. If Statments \n --------------" << endl;
+    int condition = 4;
+    int test = 4;
+
+    if(test >= condition){
+        cout << "if test for int success" << endl;
+    } else {
+        cout << "if test for int fail" << endl;
+
+    }
+
+    return 0;
+}
+
+void cOutTest(){
+
     // print out to same line
+    cout << " 1. C OUT STREAM \n --------------" << endl;
     cout << "Hello ";
     cout << "World \n";
 
@@ -19,5 +49,17 @@ int main() {
     // print out to new line
     cout << "Hello" << endl;
     cout << " \t  World";
-    return 0;
+
+    cout << "\n" << endl;
+
+}
+
+void variablesTest(){
+    cout << " 2. Variables \n --------------" << endl;
+    int testVariable = 6;
+    cout << "Variable:\t";
+    cout << testVariable << endl;
+
+    cout << "\n" << endl;
+
 }
