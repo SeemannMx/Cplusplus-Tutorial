1eb6666d6a8679cf7c51ec6e89a74bf3fcaa494a
Tino Kallinich <tkallinich@inovex.de>
2017-11-23 14:48:40 +0100

    object and classes, define classes always over main or in header file

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ void cOutTest();
 void variablesTest();
 void ifElseTest();
 int calcAge(int yearBorn, int yearToday);
+void callMyClass();
+
+// classes shuld be in header file or above main cause main needs to know it's classes
+// create Class with one public function
+class MyClass{
+    // access specifier (private, proteced, public)
+    public:
+        // declared public function in MyClass
+        void testMyClass(){
+            cout << "test MyClass" << endl;
+        }
+};
 
 /**
  * start main in C++ to start tutorial
@@ -14,7 +26,7 @@ int calcAge(int yearBorn, int yearToday);
  * @return
  */
 int main() {
-    // 1. C OUT STREAM
+    // 1. c out stream
     cOutTest();
 
     // 2. Variables
@@ -23,10 +35,13 @@ int main() {
     // 8. If Statments
    ifElseTest();
 
-    //11. function with paramters
+    // 10. function with paramters
     int x = 1984;
     int y = 2017;
     calcAge(x, y);
+
+    // 12. Objects and Classes (group functions in class)
+    callMyClass();
 
     return 0;
 }
@@ -34,7 +49,7 @@ int main() {
 void cOutTest(){
 
     // print out to same line
-    cout << " 1. c out stream \n --------------" << endl;
+    cout << " 1. c out stream \n ----------------------------" << endl;
     cout << "Hello ";
     cout << "World \n";
 
@@ -50,7 +65,7 @@ void cOutTest(){
 }
 
 void variablesTest(){
-    cout << " 2. Variables \n --------------" << endl;
+    cout << " 2. Variables \n ----------------------------" << endl;
     int testVariable = 6;
     cout << "Variable:\t";
     cout << testVariable << endl;
@@ -60,7 +75,7 @@ void variablesTest(){
 }
 
 void ifElseTest(){
-    cout << " 8. If Statments \n --------------" << endl;
+    cout << " 8. If Statments \n ----------------------------" << endl;
     int condition = 4;
     int test = 4;
 
@@ -76,7 +91,7 @@ void ifElseTest(){
 }
 
 int calcAge(int yearBorn, int yearToday){
-    cout << " 11. function with param \n --------------" << endl;
+    cout << " 11. function with param \n ----------------------------" << endl;
     int result = yearToday - yearBorn;
 
     cout << "calculated age is: " << result << endl;
@@ -84,6 +99,17 @@ int calcAge(int yearBorn, int yearToday){
     cout << "\n" << endl;
     return result;
 
+}
 
+void callMyClass(){
+    cout << " 12. object & classes \n ----------------------------" << endl;
+
+    // create a object of MyClass and call functions declared in MyClass
+    MyClass myClassObject;
+    myClassObject.testMyClass();
+
+    cout << "\n" << endl;
 
 }
+
+
