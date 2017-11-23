2cd51f72b3907ce8b78babf52f47200421fb6efd
Tino Kallinich <tkallinich@inovex.de>
2017-11-23 14:01:43 +0100

    COUT: handle c out stream on line and create new line / stream

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
+// using standard library
+using namespace std;
+
 /**
  * start main in C++ to start tutorial
  *
  * @return
  */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // print out to same line
+    cout << "Hello ";
+    cout << "World \n";
+
+    // blank line
+    cout << "\n";
+
+    // print out to new line
+    cout << "Hello" << endl;
+    cout << " \t  World";
     return 0;
 }
